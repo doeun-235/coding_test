6a4866bc0bfe980ac96a3a0d5ddd1785fb803f73
Doeun Oh <doeun.235@gmail.com>
2023-08-14 17:59:16 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -25,12 +25,21 @@ def solution(board):
     O_len, X_len = len(O_set), len(X_set)
     O_line, X_line = check_line(O_set), check_line(X_set)
     turn_diff = O_len - X_len
-    if O_line == 2 :
-        if not X_line and O_len == 5 and X_len == 4 : return 1
-        else : return 0
-    if (O_line + X_line) > 1 : return 0
-    if O_line == 1 and turn_diff == 1: return 1 
-    if X_line == 1 and turn_diff == 0: return 1
-    if not check_line(X_set) and not check_line(O_set):
-        if turn_diff in [0,1] : return 1
-    return 0
+    
+    if turn_diff == 0 :
+        if O_line : return 0
+        else : return 1
+    elif turn_diff == 1:
+        if X_line : return 0
+        else : return 1
+    else : return 0
+    
+#    if O_line == 2 :
+#        if not X_line and turn_diff == 1 : return 1
+#        else : return 0
+#    if (O_line + X_line) > 1 : return 0
+#    if O_line == 1 and turn_diff == 1: return 1 
+#    if X_line == 1 and turn_diff == 0: return 1
+#    if not check_line(X_set) and not check_line(O_set):
+#        if turn_diff in [0,1] : return 1
+#    return 0
